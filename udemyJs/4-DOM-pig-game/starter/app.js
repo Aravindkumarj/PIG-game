3a0d4e62c7a269cfde9b5f21b581cb7c423519fb
Aravind Kumar Jagannathan <aravind.kumar.jagannathan@sap.com>
2020-06-13 22:17:09 -0500

    Pig_Game_06_13_Good

--- udemyJs/4-DOM-pig-game/starter/app.js
+++ udemyJs/4-DOM-pig-game/starter/app.js
@@ -11,38 +11,194 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
+scores = [90, 90];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
+//set initial dice image to none
 document.querySelector('.dice').style.display = 'none';
 
-function btn(){
-	// do something here
+// set initial values to 0
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+//event listener
+
+//Initialization function
+
+function initialize(){
+	
+	//initialize all variables
+	scores = [90 , 90]
+	roundScore = 0;
+	activePlayer = 0;
+	
+//	document.getElementById('score-0').textContent = '0';
+//	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	
+	//set initial dice image to none
+	document.querySelector('.dice').style.display = 'none';
+	
+	// set active panel to player 0
+	//remove active panel from current player
+	document.querySelector('.player-1-panel').classList.remove('active');
+		
+	//switch player
+	activePlayer = 0;
+		
+	// add active panel to the current player
+	document.querySelector('.player-0-panel').classList.add('active');
 }
 
+function newGame(){
+	
+	//initialize all variables
+	scores = [90 , 90]
+	roundScore = 0;
+	activePlayer = 0;
+	
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	
+	//set initial dice image to none
+	document.querySelector('.dice').style.display = 'none';
+	
+	// set active panel to player 0
+	//remove active panel from current player
+	document.querySelector('.player-1-panel').classList.remove('active');
+		
+	//switch player
+	activePlayer = 0;
+		
+	// add active panel to the current player
+	document.querySelector('.player-0-panel').classList.add('active');
+	
+	// reset 'Winner' to player
+	document.querySelector('#name-0').textContent = 'Player 1';
+	document.querySelector('#name-1').textContent = 'Player 2';
+	
+	//set the roll dice button active
+	document.querySelector('.btn-roll').disabled = false;
+}
+
+//anonymous function
 document.querySelector('.btn-roll').addEventListener('click', function(){
 	
-	//1. Random number
+	// 1. random number
 	var dice = Math.floor(Math.random() * 6) + 1;
 	
-	// 2. display number
-	document.querySelector('.dice').style.display = 'block';
+	// 2. display result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+	
+	// 3. Update round score only if the rolled number is not equal to 1
+	if (dice != 1){
+		//calculate round score
+		roundScore = roundScore + dice;
+		
+		// show round score on the page
+		document.querySelector('#current-'+activePlayer).textContent = roundScore;
+		
+	}
+	else {
+		
+		//reset round score
+		roundScore = 0;
+	
+		//add current score to total score
+		scores[activePlayer] = scores[activePlayer] + roundScore;
+	
+		//reset current score to 0
+		document.querySelector('#current-'+activePlayer).textContent = '0';
+	
+		//reset dice image to none.
+		//document.querySelector('.dice').style.display = 'none'; 
+		
+		//remove active panel from current player
+		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+		
+		//switch player
+		activePlayer = 1 - activePlayer;
+		
+		// add active panel to the current player
+		document.querySelector('.player-'+activePlayer+'-panel').classList.add('active');
+	}
+	
+	// activate hold button
+	document.querySelector('.btn-hold').disabled = false;
 
-	// 3. update round score only if the rolled number is not 1
+		
+});
 
+// pushing the hold button
+document.querySelector('.btn-hold').addEventListener('click',function(){
+	
+	//add current score to total score
+	scores[activePlayer] = scores[activePlayer] + roundScore;
+	
+	//reset current score to 0
+	roundScore = 0;
+	var currentDom = document.querySelector('.player-current-score');
+		currentDom.textContent = 0;
+	
+	//reset dice image to none.
+	document.querySelector('.dice').style.display = 'none';
+	
+	// display total score 
+	document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
+	
+	// check if total score is 100
+	if (scores[activePlayer] >= 100){
+		document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+		document.querySelector('.player-0-panel').classList.remove('active');
+		document.querySelector('.player-1-panel').classList.remove('active');
+		
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+		
+		//set initial dice image to none
+		document.querySelector('.dice').style.display = 'none';
+		
+		// set active panel to player 0
+		//remove active panel from current player
+		document.querySelector('.player-1-panel').classList.remove('active');
+			
+		//switch player
+		activePlayer = 0;
+			
+		// deactivate roll button
+		document.querySelector('.btn-roll').disabled = true;
+		
+	}
+	else {
+		
+		//remove active panel from current player
+		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+		
+		//switch player
+		// can be written as 
+		// activePlayer === 0? activePlayer = 1
+		activePlayer = 1 - activePlayer;
+		
+		// add active panel to the current player
+		document.querySelector('.player-'+activePlayer+'-panel').classList.add('active');
+		
+		
+	}
+	
+	//you can press hold only once in a turn
+	document.querySelector('.btn-hold').disabled = true;
+	
+	
+});
 
-
-
-
-
-
-
-
-
-
-
-//read values of HTML elements.
-
-var x = document.querySelector('#score-1').textContent;
-console.log(x);
+// pushing new game buttom
+document.querySelector('.btn-new').addEventListener('click',newGame);
